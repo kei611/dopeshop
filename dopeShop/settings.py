07dada83efc63aaa5236e6494cf2fdfad4d7cd68
作間景 <sakumakei@MacBook-Pro.local>
2023-02-04 15:53:49 +0900

    allowed host

--- dopeShop/settings.py
+++ dopeShop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) 
 
-ALLOWED_HOSTS = ['dopeshop-env.eba-8mihmwqt.us-west-2.elasticbeanstalk.com', '*']
+ALLOWED_HOSTS = ['dopeshop-env.eba-rwrbf59h.us-west-2.elasticbeanstalk.com', '*']
 
 
 # Application definition
