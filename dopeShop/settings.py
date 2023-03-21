7d615aec10e012972e3fefa91aa4154e34f38f10
作間景 <sakumakei@MacBook-Pro.local>
2023-03-21 19:04:25 +0900

    rereredo modified 'sold out' tag

--- dopeShop/settings.py
+++ dopeShop/settings.py
@@ -147,8 +147,8 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
-# AWS_DEFAULT_ACL = None
+# AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
