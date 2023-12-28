8281026d86a96232b669a387716edd4248628ce3
作間景 <sakumakei@MacBook-Pro.local>
2023-12-28 14:40:48 +0900

    add terms and policy

--- dopeShop/urls.py
+++ dopeShop/urls.py
@@ -6,13 +6,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')), 
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('securelogin/', admin.site.urls),
-    # path('admin/', admin.site.urls), 
-    path('', views.home, name='home'), 
-    path('contact/', views.contact, name='contact'), 
-    path('store/', include('store.urls')), 
-    path('cart/', include('carts.urls')), 
-    
+    # path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
+    path('terms-and-privacy', views.termsAndPrivacy, name='terms-and-privacy'),
+
     # ORDERS
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dopeShop/views.py
+++ dopeShop/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
-    
+
 def contact(request):
     return render(request, 'contact.html')
+
+def termsAndPrivacy(request):
+    return render(request, 'terms_and_privacy.html')
